0f3a37fcb75126e13886e4b9fb3819ce538b0b3a
changbowen <changbowen@msn.cn>
2016-12-10 21:46:29 +0800

    adapt code in C#

--- App.xaml.cs
+++ App.xaml.cs
@@ -8,10 +8,47 @@ using System.Windows;
 
 namespace DesktopNote
 {
-    /// <summary>
-    /// App.xaml 的交互逻辑
-    /// </summary>
     public partial class App : Application
     {
+        //Private Declare Unicode Function PathIsNetworkPath Lib "shlwapi" Alias "" (ByVal pszPath As String) As Boolean
+        [System.Runtime.InteropServices.DllImport("shlwapi.dll")]
+        private static extern bool PathIsNetworkPathW(string pszPath);
+
+        private void RunCheck(object sender1, StartupEventArgs e1)
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += (object sender, ResolveEventArgs e) =>
+              {
+                  var desiredAssembly = new System.Reflection.AssemblyName(e.Name).Name;
+                  if (desiredAssembly == "Xceed.Wpf.Toolkit")
+                  {
+                      var ressourceName = "DesktopNote." + desiredAssembly + ".dll";
+                      using (var stream = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream(ressourceName))
+                      {
+                          byte[] assemblyData = new byte[stream.Length];
+                          stream.Read(assemblyData, 0, assemblyData.Length);
+                          return System.Reflection.Assembly.Load(assemblyData);
+                      }
+                  }
+                  else
+                      return null;
+              };
+
+            if (PathIsNetworkPathW(System.AppDomain.CurrentDomain.BaseDirectory))
+            {
+                MessageBox.Show("Please do not run this program from a network location. Copy to a local drive first./r/n Program will exit now.", "", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                Current.Shutdown();
+                return;
+            }
+
+            if (System.Diagnostics.Process.GetProcessesByName(System.Diagnostics.Process.GetCurrentProcess().ProcessName).Length > 1)
+            {
+                MessageBox.Show("Only one instance of DesktopNote can be running.", "", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                Current.Shutdown();
+                return;
+            }
+
+            var mainwin = new MainWindow();
+            mainwin.Show();
+        }
     }
 }
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,9 +10,9 @@ using System.Windows;
 [assembly: AssemblyTitle("DesktopNote")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Carl Chang")]
 [assembly: AssemblyProduct("DesktopNote")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // 可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
